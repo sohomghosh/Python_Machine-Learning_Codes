dc3135cf3baf45a602ef896de331f3380141b08e
Sohom Ghosh <sohom1ghosh@gmail.com>
2017-03-26 21:11:25 +0530

    adding TSNE_plot

---
+++ TSNE_plot.py
@@ -0,0 +1,10 @@
+import numpy as np
+from sklearn.manifold import TSNE
+import matplotlib.pylab as plt
+model = TSNE(n_components=2, random_state=0)
+aa=model.fit_transform(X)
+
+wrong=plt.scatter(aa[0:np.shape(X_train)[0],0],aa[0:np.shape(X_train)[0],1],color='red')
+right=plt.scatter(aa[np.shape(X_train)[0]+1:,0],aa[np.shape(X_train)[0]+1:,1],color='blue')
+plt.legend((wrong,right),('Wrong','Right'),loc='lower left',fontsize=8)
+plt.show(block=True)
