35414b3cc47c6991ed9b7edac65da81991fe5cd9
Sohom Ghosh <sohom1ghosh@gmail.com>
2017-03-26 21:35:00 +0530

    Adding wordcloud

--- WordCloud.py
+++ WordCloud.py
@@ -1,4 +1,9 @@
+import numpy as np
+import pandas as pd
+import collections
 from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
 data=pd.read_csv("file.csv")
 text=data[data['Name'] == 'DDT']
 text=data["comments"]
