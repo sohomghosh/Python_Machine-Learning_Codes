32ce3fb75de337c9102ccd05fa89d2fb76ecdb62
Sohom Ghosh <sohom1ghosh@gmail.com>
2017-05-23 20:29:35 +0530

    Update sort a dictonary.py

--- sort a dictonary.py
+++ sort a dictonary.py
@@ -1,4 +1,4 @@
 di={'aa':2,'bb':5}
- sorted_dictionary=sorted(di.items(), key=lambda x: (-x[1], x[0]))
- for a,b in sorted_dictonary:
-  print(str(a)+":::"+str(b))
+sorted_dictionary=sorted(di.items(), key=lambda x: (-x[1], x[0]))
+for a,b in sorted_dictonary:
+ print(str(a)+":::"+str(b))
