d368a738fec5d0cc3b7d25643f4ca758fbd87cb1
Sohom Ghosh <sohom1ghosh@gmail.com>
2017-05-30 16:46:16 +0530

    Update multichar_seperator_read_pandas.py

--- multichar_seperator_read_pandas.py
+++ multichar_seperator_read_pandas.py
@@ -2,4 +2,4 @@ import pandas as pd
 import numpy as np
 from io import StringIO
 
-data=pd.read_csv(StringIO(''.join(l.replace('||', ';') for l in open("data.csv"))),sep='$')
+data=pd.read_csv(StringIO(''.join(l.replace('||', '$') for l in open("data.csv"))),sep='$')
