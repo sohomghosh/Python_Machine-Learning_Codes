19feca30aa53c4646741fab6591609b8be62301d
Sohom Ghosh <sohom1ghosh@gmail.com>
2017-08-11 12:59:20 +0530

    Update sklearn_packages.py

--- sklearn_packages.py
+++ sklearn_packages.py
@@ -18,3 +18,26 @@ y = diabetes.target[:150]
 lasso = linear_model.Lasso()
 y_pred = cross_val_predict(lasso, X, y)
 
+
+
+#http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.StratifiedKFold.html
+
+from sklearn.model_selection import StratifiedKFold
+X = np.array([[1, 2], [3, 4], [1, 2], [3, 4]])
+y = np.array([0, 0, 1, 1])
+skf = StratifiedKFold(n_splits=2)
+skf.get_n_splits(X, y)
+#2
+
+print(skf)  
+#StratifiedKFold(n_splits=2, random_state=None, shuffle=False)
+
+
+for train_index, test_index in skf.split(X, y):
+	print("TRAIN:", train_index, "TEST:", test_index)
+	X_train, X_test = X[train_index], X[test_index]
+	y_train, y_test = y[train_index], y[test_index]
+
+
+#TRAIN: [1 3] TEST: [0 2]
+#TRAIN: [0 2] TEST: [1 3]
