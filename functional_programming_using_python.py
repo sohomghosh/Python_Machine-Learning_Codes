c9d2450ffbe32b74f3cdee11979f369846bd3fa3
Sohom Ghosh <sohom1ghosh@gmail.com>
2017-08-11 21:35:47 +0530

    Create functional_programming_using_python.py

---
+++ functional_programming_using_python.py
@@ -0,0 +1,2 @@
+https://www.hackerearth.com/practice/python/functional-programming/functional-programming-1/tutorial/
+https://www.hackerearth.com/practice/notes/functional-programming-a-new-paradigm/
