8a19621e35163dfc466b3bcfd104a1354084f434
Sohom Ghosh <sohom1ghosh@gmail.com>
2017-08-13 00:57:23 +0530

    Update MatPlotlibDATA_VISUALIZE.py

--- MatPlotlibDATA_VISUALIZE.py
+++ MatPlotlibDATA_VISUALIZE.py
@@ -2,4 +2,5 @@ import matplotlib.pyplot as plt
 plt.plot(k_range,scores)
 plt.xlabel('value of k for KNN')
 plt.ylabel('Testing Accuracy')
-plt.savefig('test_set_feature_relations.png',dpi=1000)#more dpi more size of the image
+plt.savefig('test_set_feature_relations.png',dpi=1000) #more dpi more size of the image
+plt.clf() #to flush out plt from memory so that new plot can be created
