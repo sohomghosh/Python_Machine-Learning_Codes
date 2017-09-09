4c53c1160073aaf1494399333355f37b4054b44e
Sohom Ghosh <sohom1ghosh@gmail.com>
2017-09-09 12:32:32 +0530

    Update WordCloud.py

--- WordCloud.py
+++ WordCloud.py
@@ -8,6 +8,11 @@ data=pd.read_csv("file.csv")
 text=data[data['Name'] == 'DDT']
 text=data["comments"]
 wordcloud = WordCloud().generate(str(text))
+
+#Arguments of WordCloud
+#['self', 'font_path', 'width', 'height', 'margin', 'ranks_only', 'prefer_horizontal', 'mask', 'scale', 'color_func', 'max_words', 'min_font_size', 'stopwords', 'random_state', 'background_color', 'max_font_size', 'font_step', 'mode', 'relative_scaling', 'regexp', 'collocations', 'colormap', 'normalize_plurals']
+
+
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.show()
