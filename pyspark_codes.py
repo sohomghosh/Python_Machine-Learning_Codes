bf825bc77ed4263dacc41bc38135dbbc7e78cfac
Sohom Ghosh <sohom1ghosh@gmail.com>
2017-09-17 13:02:33 +0530

    Update pyspark_codes.py

--- pyspark_codes.py
+++ pyspark_codes.py
@@ -297,3 +297,30 @@ train_test=train_test.na.fill({'siteid':3696590,'browserid_merged':2, 'devid_enc
 #Sellect not null values of a column
 df1.filter(df1.ColumnName_to_check.isNotNull()).show
 
+
+
+
+#UDF return multiple columns
+#Source: https://stackoverflow.com/questions/35322764/apache-spark-assign-the-result-of-udf-to-multiple-dataframe-columns
+from pyspark.sql.functions import udf
+from pyspark.sql.types import *
+
+schema = StructType([
+    StructField("foo", FloatType(), False),
+    StructField("bar", FloatType(), False)
+])
+
+def udf_test(n):
+    return (n / 2, n % 2) if n and n != 0.0 else (float('nan'), float('nan'))
+
+test_udf = udf(udf_test, schema)
+df = sc.parallelize([(1, 2.0), (2, 3.0)]).toDF(["x", "y"])
+
+foobars = df.select(test_udf("y").alias("foobar"))
+foobars.printSchema()
+## root
+##  |-- foobar: struct (nullable = true)
+##  |    |-- foo: float (nullable = false)
+##  |    |-- bar: float (nullable = false)
+
+foobars.select("foobar.foo", "foobar.bar").show()
