b39c58e90697a57ac2641b02b04d23477e7a478d
Sohom Ghosh <sohom1ghosh@gmail.com>
2017-12-04 12:51:05 +0530

    Update 01_pyspark_codes.py

--- 01_pyspark_codes.py
+++ 01_pyspark_codes.py
@@ -415,3 +415,23 @@ new_log_df.cache().withColumn("timePeriod", encodeUDF(new_log_df["START_TIME"]))
      func.count(func.lit(1)).alias("Num Of Records")
    )
   .show(20, False)
+
+
+#Filtering using udf : Source: https://gist.github.com/samuelsmal/feb86d4bdd9a658c122a706f26ba7e1e
+from pyspark.sql.functions import udf
+from pyspark.sql.types import BooleanType
+
+def regex_filter(x):
+    regexs = ['.*ALLYOURBASEBELONGTOUS.*']
+    
+    if x and x.strip():
+        for r in regexs:
+            if re.match(r, x, re.IGNORECASE):
+                return True
+    
+    return False 
+    
+    
+filter_udf = udf(regex_filter, BooleanType())
+
+df_filtered = df.filter(filter_udf(df.field_to_filter_on))
