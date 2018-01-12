e39dcd544650400402e492db3e403e33e9db07b4
Sohom Ghosh <sohom1ghosh@gmail.com>
2018-01-12 15:49:36 +0530

    Update 02_EDA_MisingValue_Treatment_Testing_Feature_Engg_code.py

--- 02_EDA_MisingValue_Treatment_Testing_Feature_Engg_code.py
+++ 02_EDA_MisingValue_Treatment_Testing_Feature_Engg_code.py
@@ -343,6 +343,42 @@ onehot_df = one_hot_encode_fns(col_to_encode,train)
 train = pd.concat([train, onehot_df], axis = 1)
 del train[col_to_encode]
 
+####Targeting Encoding
+#Source: https://www.kaggle.com/ogrellier/python-target-encoding-for-categorical-features
+#Theory: http://www.saedsayad.com/encoding.htm
+def add_noise(series, noise_level):
+    return series * (1 + noise_level * np.random.randn(len(series)))
+
+def target_encode(trn_series=None, tst_series=None, target=None, min_samples_leaf=1, smoothing=1, noise_level=0):
+    assert len(trn_series) == len(target)
+    assert trn_series.name == tst_series.name
+    temp = pd.concat([trn_series, target], axis=1)
+    # Compute target mean 
+    averages = temp.groupby(by=trn_series.name)[target.name].agg(["mean", "count"])
+    # Compute smoothing
+    smoothing = 1 / (1 + np.exp(-(averages["count"] - min_samples_leaf) / smoothing))
+    # Apply average function to all target data
+    prior = target.mean()
+    # The bigger the count the less full_avg is taken into account
+    averages[target.name] = prior * (1 - smoothing) + averages["mean"] * smoothing
+    averages.drop(["mean", "count"], axis=1, inplace=True)
+    # Apply averages to trn and tst series
+    ft_trn_series = pd.merge(
+        trn_series.to_frame(trn_series.name),
+        averages.reset_index().rename(columns={'index': target.name, target.name: 'average'}),
+        on=trn_series.name,
+        how='left')['average'].rename(trn_series.name + '_mean').fillna(prior)
+    # pd.merge does not keep the index so restore it
+    ft_trn_series.index = trn_series.index 
+    ft_tst_series = pd.merge(
+        tst_series.to_frame(tst_series.name),
+        averages.reset_index().rename(columns={'index': target.name, target.name: 'average'}),
+        on=tst_series.name,
+        how='left')['average'].rename(trn_series.name + '_mean').fillna(prior)
+    # pd.merge does not keep the index so restore it
+    ft_tst_series.index = tst_series.index
+    return add_noise(ft_trn_series, noise_level), add_noise(ft_tst_series, noise_level)
+
 
 '''
 #BETTER APPROACH: Distribute data among train and valid
