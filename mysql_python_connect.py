844e55af9b9ef0d4342c685e729e0c37b2b6bd01
Sohom Ghosh <sohom1ghosh@gmail.com>
2018-02-01 12:32:20 +0530

    Update mysql_python_connect.py

--- mysql_python_connect.py
+++ mysql_python_connect.py
@@ -15,3 +15,18 @@ for row in cur.fetchall():
  
 
 db.close()
+
+
+
+#MariaDB connection
+query = 'select * from table_name'
+query='Insert into table_name values(%s,%s)'%(value_1,value_2)
+try:
+    conn= pymysql.connect(host='<ip_175.55.00.229>',user='user_id',password='password',db='database_name')
+    if conn:
+        print('Connected to MySQL database / MariaDB')
+    cursor=conn.cursor()
+    cursor.execute(query)
+    conn.commit()
+except Error as e:
+        print('Error in Mysql Query / MariaDB / Connection')
