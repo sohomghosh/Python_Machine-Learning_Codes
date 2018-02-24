8005bd02c2b9467e54f93c7ebf224c9746db21ee
Sohom Ghosh <sohom1ghosh@gmail.com>
2018-02-24 14:09:28 +0530

    Update TSNE_plot.py

--- TSNE_plot.py
+++ TSNE_plot.py
@@ -4,6 +4,16 @@ import matplotlib.pylab as plt
 model = TSNE(n_components=2, random_state=0)
 aa=model.fit_transform(X)
 
+
+model = TSNE(n_components=2, random_state=0)
+aa = model.fit_transform(vecs_of_nodes)
+
+plt.scatter(aa[:,0],aa[:,1])
+plt.legend(loc='lower left',fontsize=8)
+#plt.show(block=True)
+
+
+##PLOT MORE THAN ONE
 wrong=plt.scatter(aa[0:np.shape(X_train)[0],0],aa[0:np.shape(X_train)[0],1],color='red')
 right=plt.scatter(aa[np.shape(X_train)[0]:,0],aa[np.shape(X_train)[0]:,1],color='blue')
 plt.legend((wrong,right),('Wrong','Right'),loc='lower left',fontsize=8)
