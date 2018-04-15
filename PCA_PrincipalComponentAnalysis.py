1a3a2833aa2297ccfdb645b3bf5f46650e1fce34
Sohom Ghosh <sohom1ghosh@gmail.com>
2018-04-15 19:32:34 +0530

    Create PCA_PrincipalComponentAnalysis.py

---
+++ PCA_PrincipalComponentAnalysis.py
@@ -0,0 +1,47 @@
+#Reference:   https://machinelearningmastery.com/calculate-principal-component-analysis-scratch-python/
+
+#Using package PCA
+from numpy import array
+from sklearn.decomposition import PCA
+# define a matrix
+A = array([[1, 2], [3, 4], [5, 6]])
+print(A)
+# create the PCA instance
+pca = PCA(2) #2 is the number of principal components we want
+# fit on data
+pca.fit(A)
+# access values and vectors
+print(pca.components_)
+print(pca.explained_variance_)
+# transform data
+B = pca.transform(A)
+print(B) #B is the final transformed data which is to be used
+
+
+#################################################################################################################
+
+
+from numpy import array
+from numpy import mean
+from numpy import cov
+from numpy.linalg import eig
+# define a matrix
+A = array([[1, 2], [3, 4], [5, 6]])
+print(A)
+# calculate the mean of each column
+M = mean(A.T, axis=1)
+print(M)
+# center columns by subtracting column means
+C = A - M
+print(C)
+# calculate covariance matrix of centered matrix
+V = cov(C.T)
+print(V)
+# eigendecomposition of covariance matrix
+values, vectors = eig(V)
+print(vectors)
+print(values)
+# project data
+P = vectors.T.dot(C.T)
+print(P.T)
+
