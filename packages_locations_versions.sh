01e54d3976769e523accc90edf175417a8e0e20a
Sohom Ghosh <sohom1ghosh@gmail.com>
2018-05-28 17:56:39 +0530

    Create packages_locations_versions.sh

---
+++ packages_locations_versions.sh
@@ -0,0 +1,11 @@
+#Show all packages along with their versions
+pip list | xargs -exec pip show
+
+/opt/anaconda3/lib/python3.6/site-packages/rasa_nlu
+
+#See all packages and their versions
+pip freeze
+
+#Manually
+>import keras
+>keras.__version__
