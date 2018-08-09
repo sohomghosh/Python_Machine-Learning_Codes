deffcc3c05f20b5dc94b231db2eb2bd65d0d2880
Sohom Ghosh <sohom1ghosh@gmail.com>
2018-08-09 22:10:53 +0530

    Update light_gbm.py

--- light_gbm.py
+++ light_gbm.py
@@ -8,3 +8,45 @@ X_test = <pandas dataframe>
 gbm = lgb.LGBMClassifier(n_estimators=2900, max_depth=3, subsample=0.7, colsample_bytree= 0.7)
 gbm = gbm.fit(X_train, y_train)
 y_test = gbm.predict_proba(X_Test)
+
+
+
+#### BETTER METHOD #### Refernce: https://www.kaggle.com/samratp/lightgbm-xgboost-catboost
+def run_lgb(train_X, train_y, val_X, val_y, test_X):
+    params = {
+        "objective" : "regression",
+        "metric" : "rmse",
+        "num_leaves" : 40,
+        "learning_rate" : 0.005,
+        "bagging_fraction" : 0.6,
+        "feature_fraction" : 0.6,
+        "bagging_frequency" : 6,
+        "bagging_seed" : 42,
+        "verbosity" : -1,
+        "seed": 42
+    }
+    
+    lgtrain = lgb.Dataset(train_X, label=train_y)
+    lgval = lgb.Dataset(val_X, label=val_y)
+    evals_result = {}
+    model = lgb.train(params, lgtrain, 5000, 
+                      valid_sets=[lgtrain, lgval], 
+                      early_stopping_rounds=100, 
+                      verbose_eval=150, 
+                      evals_result=evals_result)
+    
+    pred_test_y = np.expm1(model.predict(test_X, num_iteration=model.best_iteration))
+    return pred_test_y, model, evals_resul
+
+
+pred_test, model, evals_result = run_lgb(dev_X, dev_y, val_X, val_y, X_test)
+print("LightGBM Training Completed...")
+
+
+# feature importance
+print("Features Importance...")
+gain = model.feature_importance('gain')
+featureimp = pd.DataFrame({'feature':model.feature_name(), 
+                   'split':model.feature_importance('split'), 
+                   'gain':100 * gain / gain.sum()}).sort_values('gain', ascending=False)
+print(featureimp[:50])
