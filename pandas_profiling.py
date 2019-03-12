bbd4f3c331d6b44948d596d4721354d991213e2b
Sohom Ghosh <sohom1ghosh@gmail.com>
2019-03-12 16:24:59 +0530

    Create pandas_profiling.py

---
+++ pandas_profiling.py
@@ -0,0 +1,6 @@
+import pandas_profiling 
+#pip3 install pandas-profiling #Remember only X vaiables of training set is to be given as input in pandas profiling
+#pandas_profiling.ProfileReport(X_train) #For IPython Notebook
+profile = pandas_profiling.ProfileReport(X_train)
+rejected_variables = profile.get_rejected_variables(threshold=0.9)
+profile.to_file(outputfile="pandas_profiling_output_train.html")
