98ede39a2e9b2b08b4f9fd36b4a0e6f7813894b2
Sohom Ghosh <sohom1ghosh@gmail.com>
2019-08-20 10:12:18 +0530

    Update pandas_profiling.py

--- pandas_profiling.py
+++ pandas_profiling.py
@@ -3,4 +3,4 @@ import pandas_profiling
 #pandas_profiling.ProfileReport(X_train) #For IPython Notebook
 profile = pandas_profiling.ProfileReport(X_train)
 rejected_variables = profile.get_rejected_variables(threshold=0.9)
-profile.to_file(outputfile="pandas_profiling_output_train.html")
+profile.to_file(output_file="pandas_profiling_output_train.html")
