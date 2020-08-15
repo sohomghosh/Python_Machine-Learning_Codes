0cf4637f839cc958cb4711361b9300058c7ad8e5
Sohom Ghosh <sohom1ghosh@gmail.com>
2020-08-15 19:49:23 +0530

    Create adding_new_environment_to_jupyter_notebook.sh

---
+++ adding_new_environment_to_jupyter_notebook.sh
@@ -0,0 +1,3 @@
+# To add new environment to jupyter
+(new_env)C:\Users> python -m ipykernel install --name=new_env
+#Remember this is to be preferably done after: conda activate new_env; It will search for new_env in the environment list.
